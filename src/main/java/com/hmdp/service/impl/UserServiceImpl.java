e17514ea9868d60c59085c73bc750fdac6d15b78
Ian Wang <iw5420@gmail.com>
2023-04-21 14:07:47 +0800

    [發送短信驗證碼][流程]

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -17,4 +20,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1.校驗手機號
+
+        // 2.如果不符合, 返回錯誤信息
+
+        // 3.符合, 生成驗證碼
+
+        // 4.保存驗證碼到session
+
+        // 5.發送驗證碼
+
+        //返回ok
+        return Result.ok();
+    }
 }
