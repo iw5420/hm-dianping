e3069e8deab2fef0cdcf3cfe63729f62cb6e3788
Ian Wang <iw5420@gmail.com>
2023-04-21 16:54:25 +0800

    [短信驗證碼登入, 註冊][redis][流程]

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -11,10 +11,16 @@ import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_CODE_KEY;
+import static com.hmdp.utils.RedisConstants.LOGIN_CODE_TTL;
 import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
@@ -29,6 +35,9 @@ import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public Result sendCode(String phone, HttpSession session) {
         // 1.校驗手機號
@@ -39,8 +48,10 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         // 3.符合, 生成驗證碼 6位數字
         String code = RandomUtil.randomNumbers(6);
 
-        // 4.保存驗證碼到session
-        session.setAttribute("code", code);
+        // 4.保存驗證碼到redis // set key value ex 120
+        // key加入業務前綴以免衝突
+        // 設置有效期, 避免被惡意攻擊塞滿
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
 
         // 5.發送驗證碼
         log.debug("發送短信驗證碼成功, 驗證碼: {}", code);
@@ -56,11 +67,11 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             // 2.如果不符合, 返回錯誤信息
             return Result.fail("手機號格式錯誤");
         }
-        // 2.校驗驗證碼
+        // TODO 3.從redis獲取校驗驗證碼
         Object cacheCode = session.getAttribute("code");
         String code = loginForm.getCode();
         if(cacheCode == null || !cacheCode.toString().equals(code)){
-            // 3.不一致, 報錯-反向校驗, 避免嵌套
+            // 不一致, 報錯-反向校驗, 避免嵌套
             return Result.fail("驗證碼錯誤");
         }
         // 4.一致, 根據手機號查詢用戶
@@ -72,7 +83,11 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             user = createUserWithPhone(phone);
         }
 
-        // 7.保存用戶信息到session中
+        // TODO 7.保存用戶信息到redis中
+        // TODO 7.1.隨機生成token, 作為登陸令牌
+        // TODO 7.2.從User對象轉為Hash存儲
+        // TODO 7.3.存儲
+        // TODO 8.返回token
         session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
         return Result.ok();
     }
