360ec8b52b1162f32358eb6169f4ef4147add83c
Ian Wang <iw5420@gmail.com>
2023-04-24 14:16:13 +0800

    [緩存穿透][code] 利用寫入空值 解決緩存穿透問題, 並有用互斥鎖解決緩存擊穿問題的流程

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -15,8 +16,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -33,25 +33,86 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     private StringRedisTemplate stringRedisTemplate;
     @Override
     public Result queryById(Long id) {
+        // 緩存穿透
+        // Shop shop = queryWithPassThrough(id);
+        Shop shop = queryWithMutex(id);
+        return Result.ok(shop);
+    }
+
+    public Shop queryWithMutex(Long id){
         String key = CACHE_SHOP_KEY + id;
         // 1.從redis查詢商鋪緩存
         String shopJson = stringRedisTemplate.opsForValue().get(key);
         // 2.判斷是否存在
         if(StrUtil.isNotBlank(shopJson)){
             // 3.存在, 直接返回
-            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        if(shopJson !=null){
+            // 返回一個錯誤信息
+            return null;
+        }
+        // 4.實現緩存重建
+        // 4.1.獲取互斥鎖
+        // 4.2.判斷是否獲取成功
+        // 4.3.失敗, 則休眠並重試
+
+        // 4.4.成功, 根據id查詢資料庫
+        Shop shop = getById(id);
+        // 5.不存在, 返回錯誤
+        if(shop == null){
+            // 將空值寫入redis
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+            // 返回錯誤信息
+            return null;
+        }
+        // 6.存在, 寫入redis
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
+        // 7.釋放互斥鎖
+        // 8.返回
+        return shop;
+    }
+
+
+    // 緩存穿透
+    public Shop queryWithPassThrough(Long id){
+        String key = CACHE_SHOP_KEY + id;
+        // 1.從redis查詢商鋪緩存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2.判斷是否存在
+        if(StrUtil.isNotBlank(shopJson)){
+            // 3.存在, 直接返回
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        if(shopJson !=null){
+            // 返回一個錯誤信息
+            return null;
         }
         // 4.不存在, 根據id查詢資料庫
         Shop shop = getById(id);
         // 5.不存在, 返回錯誤
         if(shop == null){
-            return Result.fail("店鋪不存在");
+            // 將空值寫入redis
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+            // 返回錯誤信息
+            return null;
         }
         // 6.存在, 寫入redis
         stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
         // 7.返回
-        return Result.ok(shop);
+        return shop;
+    }
+
+    private boolean tryLock(String key){
+        //10秒
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        //避免自動拆箱產生空指針
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unlock(String key){
+        stringRedisTemplate.delete(key);
     }
 
     @Override
