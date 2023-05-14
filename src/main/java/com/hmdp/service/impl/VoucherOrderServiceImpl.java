749181a59197e4f8317177010179b9ea4f6fd9e4
Ian Wang <iw5420@gmail.com>
2023-05-14 11:56:07 +0800

    [達人探店][code]查看探店筆記

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -78,7 +78,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         public void run() {
             while (true){
                 try{
-                    // 1.獲取隊列中的訂單信息 XREADCROUP CROUP g1 c1 COUNT 1 BLOCK 2000 streams.order >
+                    // 1.獲取隊列中的訂單信息 XREADCROUP GROUP g1 c1 COUNT 1 BLOCK 2000 streams.order >
                     List<MapRecord<String, Object, Object>> list = stringRedisTemplate.opsForStream().read(
                             Consumer.from("g1", "c1"),
                             StreamReadOptions.empty().count(1).block(Duration.ofSeconds(2)),
@@ -112,6 +112,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
                     List<MapRecord<String, Object, Object>> list = stringRedisTemplate.opsForStream().read(
                             Consumer.from("g1", "c1"),
                             StreamReadOptions.empty().count(1),
+                            // 0 表示讀的部分為 pending-list
                             StreamOffset.create(queueName, ReadOffset.from("0"))
                     );
                     // 2.判斷消息獲取是否成功
